452e8d3a79acf6081deb060c1f108807b1c60029
manoj <manojmahanta1998@gmail.com>
2023-11-29 00:19:57 +0530

    AuthController committed

---
+++ src/main/java/com/electronic/store/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.electronic.store.controllers;
+
+import com.electronic.store.dtos.UserDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private ModelMapper modelMapper;
+    @GetMapping("/current")
+    public ResponseEntity<UserDto> getCurrentUser(Principal principal){
+        String name = principal.getName();
+        return new ResponseEntity<>(modelMapper.map(userDetailsService.loadUserByUsername(name),UserDto.class), HttpStatus.OK);
+    }
+}
