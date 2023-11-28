9ae3bba894171159b7cf05aa5a79e0f261473b66
manoj <manojmahanta1998@gmail.com>
2023-11-29 00:21:39 +0530

    user entity modified

--- src/main/java/com/electronic/store/entities/User.java
+++ src/main/java/com/electronic/store/entities/User.java
@@ -2,7 +2,10 @@ package com.electronic.store.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -12,14 +15,14 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     private String userId;
     @Column(name = "user_name",length = 100)
     private String name;
     @Column(name = "user_email",unique = true)
     private String email;
-    @Column(name = "user_password",length = 10)
+    @Column(name = "user_password",length = 500)
     private String password;
     private String gender;
     @Column(length = 1000)
@@ -28,4 +31,39 @@ public class User {
     private String imageName;
     @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
     private List<Order> orders;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
